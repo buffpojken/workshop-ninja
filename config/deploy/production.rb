1e8fc8e477d2914bd8f964f577c7035f08cc02e9
buffpojken <daniel@sykewarrior.com>
2015-09-16 16:31:21 +0200

    tweaks

---
+++ config/deploy/production.rb
@@ -0,0 +1,9 @@
+set :rvm_type, :system
+set :rvm_ruby_version, "2.2.1"
+set :stage, :production
+set :server_name, "31.192.224.41"
+set :appdir, "/sites/#{fetch(:application)}"
+set :deploy_to, "#{fetch(:appdir)}"
+set :rails_env, "production"
+
+server '31.192.224.41', user: 'buffpojken', roles: %w{web app}
